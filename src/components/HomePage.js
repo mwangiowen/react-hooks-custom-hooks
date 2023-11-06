0eb88e1cb52a4d143d9357f76930318cf97c403c
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-11-06 21:39:32 +0300

    DONE

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useQuery from "../hooks/useQuery";
 
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
+  const { data: posts, isLoaded } = useQuery("http://localhost:4000/posts");
 
   useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
+    // This is unnecessary because you're already setting 'isLoaded' inside the 'useQuery' hook.
+    // setIsLoaded(false);
+    // fetch("http://localhost:4000/posts")
+    //   .then((r) => r.json())
+    //   .then((posts) => {
+    //     setPosts(posts);
+    //     setIsLoaded(true);
+    //   });
   }, []);
 
   // set the document title
